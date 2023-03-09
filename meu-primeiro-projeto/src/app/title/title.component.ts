33cc3857fcdd2ddac30d907ad78d799fec76d1a8
Esley <esleynathan@hotmail.com>
2023-03-08 22:59:05 -0300

    Componentes no Angular - ngOnChanges

--- meu-primeiro-projeto/src/app/title/title.component.ts
+++ meu-primeiro-projeto/src/app/title/title.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent {
-  public title: string = "Bem vindo!";
+export class TitleComponent implements OnInit, OnChanges {
+  
+  @Input() public title: string = "Bem vindo!";
+
+  constructor() { }
+
+  ngOnInit (): void { }
+
+  ngOnChanges(): void { 
+    alert("Foi alterado com sucesso.");
+  }
 
 }
