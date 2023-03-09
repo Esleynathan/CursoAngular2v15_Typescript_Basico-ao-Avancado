0fcf89f801d9fe9212bad026a379375b209683fe
Esley <esleynathan@hotmail.com>
2023-03-08 22:42:46 -0300

    Componentes no Angular - ngOnInit

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() { }
+
+ngOnInit(): void {
+  // console.log(1);
+
+  setTimeout( () => {
+    console.log(1);
+  }, 5000)
+}
+
 }
