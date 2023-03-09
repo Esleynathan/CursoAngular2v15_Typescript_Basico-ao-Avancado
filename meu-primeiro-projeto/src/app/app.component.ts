8bd6ab95553d450b2082d7b81569e9442be452fa
Esley <esleynathan@hotmail.com>
2023-03-08 23:28:38 -0300

    Componentes no Angular - ngDoCheck

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-title title="Olá Mundo"></app-title>
+    {{valor}}
+    <button (click)="adicionar()">Adicionar</button>
     <router-outlet></router-outlet>
   `,
 })
-export class AppComponent implements OnInit {
-  constructor() { }
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
 
-ngOnInit(): void { }
+  public valor: number = 1;
+
+  constructor() {
+  }
+  
+  public adicionar(): number{
+    return this.valor += 1;
+  }
+  ngOnInit(): void { }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
 
 }
+
