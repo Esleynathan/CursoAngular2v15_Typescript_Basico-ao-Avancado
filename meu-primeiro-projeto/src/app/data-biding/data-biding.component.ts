df445b6e2e0bc7b1b8e3c4d120e2db2f47a780c0
Esley <esleynathan@hotmail.com>
2023-03-10 23:19:42 -0300

    Componentes do Angular - Event Binding

--- meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
+++ meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
@@ -12,8 +12,18 @@ export class DataBidingComponent {
   public maisUm: number = 1;
   
   public checkedDisabled: boolean = false;
-
   public imgSrc: string = "https://w7.pngwing.com/pngs/1014/365/png-transparent-angular-js-full-logo-tech-companies.png";
-
   public imgTitle: string = 'Angular';
+  
+  public position:{x:number, y:number} = {x: 0, y: 0};
+  
+
+  public alertaInfo(valor: MouseEvent){
+    console.log(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent){
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
