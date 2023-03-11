ac0220f68bf877843c37556449071d5501afb20b
Esley <esleynathan@hotmail.com>
2023-03-10 22:58:29 -0300

    Componentes do Angular - Property Binding

--- meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
+++ meu-primeiro-projeto/src/app/data-biding/data-biding.component.ts
@@ -10,5 +10,10 @@ export class DataBidingComponent {
   public nome: string = "Esley!";
   public idade: number = 29;  
   public maisUm: number = 1;
+  
+  public checkedDisabled: boolean = false;
 
+  public imgSrc: string = "https://w7.pngwing.com/pngs/1014/365/png-transparent-angular-js-full-logo-tech-companies.png";
+
+  public imgTitle: string = 'Angular';
 }
