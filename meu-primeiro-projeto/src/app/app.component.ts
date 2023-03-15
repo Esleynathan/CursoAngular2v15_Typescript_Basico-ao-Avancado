016c0970c54ec578ec1f156323780839bd380c98
Esley <esleynathan@hotmail.com>
2023-03-14 22:44:18 -0300

    Diretivas de Atributos - ng-content

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -7,7 +7,15 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
     <!-- <app-diretivas-estruturais></app-diretivas-estruturais> -->
 
 
-    <app-diretivas-atributos> </app-diretivas-atributos>
+    <app-diretivas-atributos> 
+      <h1>Aulas de Diretivas de Atributo</h1>
+      <h3>Final da aula</h3>
+    </app-diretivas-atributos>
+    
+    <app-diretivas-atributos> 
+      <h1>Ésley Nathan</h1>
+      <h3>Camila Nascimento</h3>
+    </app-diretivas-atributos>
 
     <router-outlet></router-outlet>
   `,
